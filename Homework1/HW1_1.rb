97899002ee0dd33c0e59212c0f7c676ef6b86593
Marcio Lima <mdlima.github@mailnull.com>
2012-03-02 19:37:01 -0300

    HW1  - part 2

--- Homework1/HW1_1.rb
+++ Homework1/HW1_1.rb
@@ -1,3 +1,12 @@
 def palindrome?(string)
   string.downcase.gsub(/\W/,"").reverse == string.downcase.gsub(/\W/,"")
 end
+
+def count_words(string)
+  h = Hash.new
+
+  array = string.downcase.gsub(/\W/," ").split
+  array.uniq.map { |x| h[x]=array.count(x) }
+  
+  return h
+end
