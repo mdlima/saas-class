8a4ef7e184f0ec8772436e5d6418bd2d8309d419
Marcio Lima <mdlima.github@mailnull.com>
2012-03-07 15:51:52 -0300

    HW2 - 1a

---
+++ Homework2/hw2_1.rb
@@ -0,0 +1,19 @@
+# metaprogramming to the rescue!
+
+class Numeric
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
+  def method_missing(method_id)
+    singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self * @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+  
+  def in(currency_to)
+    singular_currency = currency_to.to_s.gsub( /s$/, '')
+    self / @@currencies[singular_currency]
+  end
+end
+
